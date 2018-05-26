584a12f02a575d1158ca3cfca7b69c4cef7dbbbb
wirelessr <ifloveyou2014@gmail.com>
2018-05-26 22:07:52 +0800

    Monthly Report

--- templateLogReg/js/accounting.js
+++ templateLogReg/js/accounting.js
@@ -10,7 +10,6 @@ function list_daily_report(isodate) {
 	var start_ts = date.getTime();
 	date.setDate(date.getDate() + 1)
 	var end_ts = date.getTime();
-	var xhttp = new XMLHttpRequest();
 	var icons = {
 		1: 'fa-cutlery',
 		2: 'fa-automobile',
@@ -20,6 +19,7 @@ function list_daily_report(isodate) {
 		6: 'fa-home',
 		99: 'fa-paperclip',
 	}
+	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var rowdata = JSON.parse(this.responseText);
@@ -48,4 +48,58 @@ function shift_day(offset) {
 	isodate = date.toISOString().slice(0,10);
 	document.getElementById("todayDate").innerHTML = isodate;
 	list_daily_report(isodate);
+}
+
+function drawChart() {
+	var isodate = (new Date()).toISOString().slice(0,7);
+	var thisMonth = new Date(isodate);
+	var start_ts = thisMonth.getTime();
+	thisMonth.setMonth(thisMonth.getMonth() + 1);
+	var end_ts = thisMonth.getTime();
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var rowdata = JSON.parse(this.responseText);
+			var summary = {}
+			var list = [['type', 'cost']]
+			var total = 0;
+			var types = {
+				1: '食物',
+				2: '交通',
+				3: '娛樂',
+				4: '醫療',
+				5: '消費',
+				6: '住家',
+				99: '其他',
+			}
+
+			for(i in rowdata.rows) {
+				if(start_ts <= rowdata.rows[i].date && rowdata.rows[i].date < end_ts) {
+					var cost = parseInt(rowdata.rows[i].cost);
+					total += cost;
+					if(summary[rowdata.rows[i].type]) {
+						summary[rowdata.rows[i].type] += cost;
+					} else {
+						summary[rowdata.rows[i].type] = cost;
+					}
+				}
+			}
+			document.getElementById("monthReport").innerHTML = '總共開銷：'+total;
+
+			for(i in summary) {
+				list.push([types[i], summary[i]]);
+			}
+			var data = google.visualization.arrayToDataTable(list);
+
+			// Optional; add a title and set the width and height of the chart
+			var options = {'title':'月結', 'width':'100%'};
+
+			// Display the chart inside the <div> element with id="piechart"
+			var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+			chart.draw(data, options);
+		}
+	};
+	xhttp.open("GET", "/list/all", true);
+	xhttp.send();
 }
